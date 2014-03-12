ca55851d307248182e23d4ce23c5d2ba4f215c79
Matt Gaunt <matt@gauntface.co.uk>
2014-03-12 18:50:04 +0000

    Adding an app icon - not happy with it, but at least it's not the Cordova one

--- cordova-app/platforms/android/ant-gen/R.java.d
+++ cordova-app/platforms/android/ant-gen/R.java.d
@@ -1,11 +1,11 @@
 /home/matt/Programming/Workspace/MiniMobileDeviceLab/cordova-app/platforms/android/ant-gen/co/uk/gauntface/mobile/devicelab/R.java \
- : /home/matt/Programming/Workspace/MiniMobileDeviceLab/cordova-app/platforms/android/res/drawable-hdpi/icon.png \
+ : /home/matt/Programming/Workspace/MiniMobileDeviceLab/cordova-app/platforms/android/res/drawable-xxhdpi/icon.png \
+/home/matt/Programming/Workspace/MiniMobileDeviceLab/cordova-app/platforms/android/res/drawable-hdpi/icon.png \
 /home/matt/Programming/Workspace/MiniMobileDeviceLab/cordova-app/platforms/android/res/values/version.xml \
 /home/matt/Programming/Workspace/MiniMobileDeviceLab/cordova-app/platforms/android/res/values/strings.xml \
 /home/matt/Programming/Workspace/MiniMobileDeviceLab/cordova-app/platforms/android/res/drawable-xhdpi/icon.png \
-/home/matt/Programming/Workspace/MiniMobileDeviceLab/cordova-app/platforms/android/res/drawable-ldpi/icon.png \
+/home/matt/Programming/Workspace/MiniMobileDeviceLab/cordova-app/platforms/android/res/drawable-xxxhdpi/icon.png \
 /home/matt/Programming/Workspace/MiniMobileDeviceLab/cordova-app/platforms/android/res/drawable-mdpi/icon.png \
-/home/matt/Programming/Workspace/MiniMobileDeviceLab/cordova-app/platforms/android/res/drawable/icon.png \
 /home/matt/Programming/Workspace/MiniMobileDeviceLab/cordova-app/platforms/android/res/xml/config.xml \
 /home/matt/Programming/Workspace/MiniMobileDeviceLab/cordova-app/platforms/android/ant-build/res/drawable-hdpi/icon.png \
 /home/matt/Programming/Workspace/MiniMobileDeviceLab/cordova-app/platforms/android/ant-build/res/drawable-xhdpi/icon.png \
