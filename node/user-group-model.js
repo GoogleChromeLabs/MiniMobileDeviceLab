26d4925d18a07ee5a45f3fe2289d461b307083d0
Matt Gaunt <matt@gauntface.co.uk>
2014-06-17 18:01:10 -0700

    Debugging for node js

--- node/user-group-model.js
+++ node/user-group-model.js
@@ -1,6 +1,7 @@
 var dbHelper = require('./db-helper');
 
 exports.getUsersInGroupWithUserId = function(userId, callback) {
+    console.log('getUsersInGroupWithUserId()');
     dbHelper.openDb(function(dbConnection) {
         dbConnection.query('SELECT group_id FROM usergroups WHERE user_id = ?', [userId],
             function (err, result) {
@@ -16,6 +17,7 @@ exports.getUsersInGroupWithUserId = function(userId, callback) {
                 }
 
                 var groupId = result[0].group_id;
+                console.log('getUsersInGroupWithUserId() groupId = '+groupId);
                 dbConnection.query('SELECT user_id FROM usergroups WHERE group_id = ?', [groupId],
                     function (err, result) {
                         if (err) {
@@ -27,6 +29,8 @@ exports.getUsersInGroupWithUserId = function(userId, callback) {
                         for(var i = 0; i < result.length; i++) {
                             userIds.push(result.user_id);
                         }
+
+                        console.log('getUsersInGroupWithUserId() userIds = '+JSON.stringify(userIds));
 
                         callback(userIds);
                         
