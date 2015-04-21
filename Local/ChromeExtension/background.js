dfba9d9dad3274f46daccadc3edfb590fc8c3fb4
Pete LePage <pete.lepage@pobox.com>
2015-04-21 17:45:33 -0400

    single quotes and save to storage

--- Local/ChromeExtension/background.js
+++ Local/ChromeExtension/background.js
@@ -1,14 +1,14 @@
 /**
  * Copyright 2015 Google Inc. All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * Licensed under the Apache License, Version 2.0 (the 'License'); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
@@ -26,21 +26,23 @@
 var settings;
 
 function init() {
-  chrome.storage.sync.get("settings", function(mmdlStorageSettings) {
-    if (mmdlStorageSettings.appID === undefined) {
-      console.log("[init] No settings found, using defaults.");
+  chrome.storage.sync.get('settings', function(mmdlStorageSettings) {
+    if (mmdlStorageSettings.settings === undefined) {
+      console.log('[init] No settings found, using defaults.');
       settings = {
-        "appID": "goog-lon-device-lab",
-        "key": "",
-        "testForServiceWorker": false,
-        "onByDefault": true
+        'appID': 'goog-lon-device-lab',
+        'key': '',
+        'testForServiceWorker': false,
+        'onByDefault': true
       }
-      chrome.storage.sync.set({"settings": settings});
+      chrome.storage.sync.set({'settings': settings});
     } else {
-      settings = mmdlStorageSettings.mmdlSettings;
-      console.log("[init]", settings);
+      settings = mmdlStorageSettings.settings;
+      console.log('[init]', settings);
     }
+
     settings.testForServiceWorker = true;
+
     if (settings.onByDefault) {
       startFirebase();
     }
@@ -52,69 +54,92 @@ init();
 /************************************************************************
  * Handle URLs
  ***********************************************************************/
+var currentTab;
 
 function openURL(url) {
-  console.log("[openURL]", url);
-  chrome.tabs.query({"index": 0}, function(tab) {
-    var tabProperties = {
-      "url": url,
-      "active": true,
-      "highlighted": true
-    };
-    chrome.tabs.update(tab.id, tabProperties, function(tab) {
-      testForServiceWorker(tab, url);
-    });
-  });
+  console.log('[openURL]', url);
 
+  var options = {
+    'url': url,
+    'active': true,
+    'index': 0
+  };
+  if (currentTab) {
+    chrome.tabs.remove(currentTab.id);
+    currentTab = null;
+  }
+
+  chrome.tabs.create(options, function(newTab) {
+    currentTab = newTab;
+    testForServiceWorker(currentTab, url);
+  });
 }
+
+/************************************************************************
+ * Test for ServiceWorkers
+ ***********************************************************************/
 
 function testForServiceWorker(tab, url) {
   if (settings.testForServiceWorker === false) {
     return;
   }
-  console.log("[testForServiceWorker]", url);
+  console.log('[testForServiceWorker]', url);
   var testCode = [
-    "var url = '[URL]';",
-    "function mmdlTestForServiceWorker() {",
-    "  console.log('running serviceWorker test...');",
-    "  if ('serviceWorker' in navigator) {",
-    "    navigator.serviceWorker.getRegistration().then(function(registration) {",
-    "      if (registration) {",
-    "        // A service worker is registered for the page.",
-    "        chrome.runtime.sendMessage({",
-    "          'serviceWorkerTest': true,",
-    "          'hasServiceWorker': true,",
-    "          'url': url",
-    "        });",
-    "      } else {",
-    "        // No service worker is registered",
-    "        chrome.runtime.sendMessage({",
-    "          'serviceWorkerTest': true,",
-    "          'hasServiceWorker': false,",
-    "          'url': url",
-    "        });",
-    "      }",
-    "    }).catch(function(err) {",
-    "      // No service worker registered because of an error",
-    "      chrome.runtime.sendMessage({",
-    "        'serviceWorkerTest': true,",
-    "        'hasServiceWorker': false,",
-    "        'error': err,",
-    "        'url': url",
-    "      });",
-    "    });",
-    "  }",
-    "}",
-    "mmdlTestForServiceWorker();"];
-  var code = testCode.join("\n");
-  code = code.replace("[URL]", url);
+    'var url = \'[URL]\';',
+    'function mmdlTestForServiceWorker() {',
+    '  console.log(\'running serviceWorker test...\');',
+    '  if (\'serviceWorker\' in navigator) {',
+    '    navigator.serviceWorker.getRegistration().then(function(registration) {',
+    '      if (registration) {',
+    '        // A service worker is registered for the page.',
+    '        chrome.runtime.sendMessage({',
+    '          \'serviceWorkerTest\': true,',
+    '          \'hasServiceWorker\': true,',
+    '          \'url\': url',
+    '        });',
+    '      } else {',
+    '        // No service worker is registered',
+    '        chrome.runtime.sendMessage({',
+    '          \'serviceWorkerTest\': true,',
+    '          \'hasServiceWorker\': false,',
+    '          \'url\': url',
+    '        });',
+    '      }',
+    '    }).catch(function(err) {',
+    '      // No service worker registered because of an error',
+    '      chrome.runtime.sendMessage({',
+    '        \'serviceWorkerTest\': true,',
+    '        \'hasServiceWorker\': false,',
+    '        \'error\': err,',
+    '        \'url\': url',
+    '      });',
+    '    });',
+    '  }',
+    '}',
+    'mmdlTestForServiceWorker();'
+  ];
+  var code = testCode.join('\n');
+  code = code.replace('[URL]', url);
   var obj = {
-    "code": code,
-    "runAt": "document_idle"
+    'code': code,
+    'runAt': 'document_idle'
   };
   setTimeout(function() {
     chrome.tabs.executeScript(tab.id, obj);
   }, 2500);
+}
+
+function saveServiceWorkerResult(url, hasServiceWorker) {
+  console.log('[saveServiceWorkerResult]', url, hasServiceWorker);
+  fbRef.child('urlkeys').orderByValue().equalTo(url).on('value', function(snapshot) {
+    var data = snapshot.val();
+    if (data) {
+      var keys = Object.keys(data);
+      var key = 'tests/' + keys[0] + '/owp/status/serviceWorker';
+      fbRef.child(key).set(hasServiceWorker);
+    }
+  });
+
 }
 
 
@@ -126,15 +151,15 @@ var fbRef;
 // Connects to Firebase with the appID and key. If no key is provided, it
 // simply uses anonymous authentication.
 function startFirebase() {
-  console.log("[startFirebase]");
+  console.log('[startFirebase]');
   if (fbRef) {
     stopFirebase();
   }
 
-  var fbURL = "https://" + settings.appID + ".firebaseio.com/";
+  var fbURL = 'https://' + settings.appID + '.firebaseio.com/';
   fbRef = new Firebase(fbURL);
   if (settings.key) {
-    console.log("[Firebase] authWithCustomToken.");
+    console.log('[Firebase] authWithCustomToken.');
     fbRef.authWithCustomToken(settings.key, onFirebaseConnected);
   } else {
     fbRef.authAnonymously(onFirebaseConnected);
@@ -142,26 +167,26 @@ function startFirebase() {
 }
 
 function stopFirebase() {
-  console.log("[stopFirebase]");
+  console.log('[stopFirebase]');
   if (fbRef) {
     fbRef.off();
     fbRef.unauth();
     fbRef = null;
   }
-  chrome.browserAction.setBadgeText({"text": ""});
+  chrome.browserAction.setBadgeText({'text': ''});
   chrome.power.releaseKeepAwake();
 }
 
 function onFirebaseConnected(err, authToken) {
-  console.log("[onFirebaseConnect]", err, authToken);
+  console.log('[onFirebaseConnect]', err, authToken);
   if (err) {
-    
+    openURL('setup.html');
   } else {
-    fbRef.child("url").on("value", function(snapshot) {
+    fbRef.child('url').on('value', function(snapshot) {
       openURL(snapshot.val());
     });
-    chrome.browserAction.setBadgeText({"text": "ON"});
-    chrome.power.requestKeepAwake("display");
+    chrome.browserAction.setBadgeText({'text': 'ON'});
+    chrome.power.requestKeepAwake('display');
   }
 }
 
@@ -183,7 +208,7 @@ chrome.browserAction.onClicked.addListener(function(tabs) {
 });
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  console.log("onMessage", request);
+  console.log('onMessage', request);
   if (request.settings) {
     settings = request.settings;
     chrome.tabs.remove(sender.tab.id);
@@ -192,8 +217,6 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       startFirebase();
     }
   } else if (request.serviceWorkerTest) {
-    console.warn("[ServiceWorker] TODO: save service worker status");
-    console.warn("[ServiceWorker] see https://github.com/GoogleChrome/MiniMobileDeviceLab/blob/next/Local/PiLab/model/URLKeyModel.js");
-    console.log("[ServiceWorker]", request.url, request.hasServiceWorker);
+    saveServiceWorkerResult(request.url, request.hasServiceWorker);
   }
 });
