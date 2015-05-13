1a85ef9407f162c06e5ae5d73bf229dff698d305
Matt Gaunt <matt@gauntface.co.uk>
2015-05-13 18:23:42 +0100

    Fixing up the install script

--- PiLab/scripts/install.sh
+++ PiLab/scripts/install.sh
@@ -5,12 +5,12 @@ sudo dpkg -i node_latest_armhf.deb
 
 git clone https://github.com/GoogleChrome/MiniMobileDeviceLab.git /home/pi/MiniMobileDeviceLab/
 
-cd /home/pi/MiniMobileDeviceLab
+cd /home/pi/MiniMobileDeviceLab/PiLab
 
 npm install
 sudo npm install -g forever
 
-sudo cp /home/pi/MiniMobileDeviceLab/scripts/startup.sh /etc/init.d/pilab-startup
+sudo cp /home/pi/MiniMobileDeviceLab/PiLab/scripts/startup.sh /etc/init.d/pilab-startup
 sudo chmod 755 /etc/init.d/pilab-startup
 
 sudo update-rc.d pilab-startup defaults
