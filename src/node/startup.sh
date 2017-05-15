9da13bcab2b00a01710f4f2965f9c2a158711af7
Matt Gaunt <matt@gauntface.co.uk>
2017-05-15 11:28:17 -0700

    Updating startup script to copy android device rules

--- src/node/startup.sh
+++ src/node/startup.sh
@@ -1,3 +1,9 @@
 # npm install
 
+# Copy over android rules.
+sudo cp ./misc/51-android.rules /etc/udev/rules.d/51-android.rules
+sudo chmod 644   /etc/udev/rules.d/51-android.rules
+sudo chown root. /etc/udev/rules.d/51-android.rules
+sudo service udev restart
+
 node ./bin.js --config ~/demo/config.json --service-account ~/demo/service-account-key.json
